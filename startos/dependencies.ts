993f6d33c64a0ec846e25e502e5970999f5c00dd
Matt Hill <mattnine@protonmail.com>
2025-03-31 12:31:17 -0600

    fix dependencies

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -1,17 +1,17 @@
 import { sdk } from './sdk'
 
 export const setDependencies = sdk.setupDependencies(async ({ effects }) => {
-  const implementation = await sdk.store
+  const LN_BACKEND_TYPE = await sdk.store
     .getOwn(effects, sdk.StorePath.LN_BACKEND_TYPE)
     .const()
 
-  if (implementation === 'LDK') return {}
-
-  return {
-    lnd: {
-      kind: 'running',
-      versionRange: '>=0.18.4:1',
-      healthChecks: ['rpc', 'synced'],
-    },
-  }
+  return LN_BACKEND_TYPE === 'LND'
+    ? {
+        lnd: {
+          kind: 'running',
+          versionRange: '>=0.18.4:1',
+          healthChecks: ['rpc', 'synced'],
+        },
+      }
+    : {}
 })
