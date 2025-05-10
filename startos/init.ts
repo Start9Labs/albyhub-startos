825889f427e0eb10eed860d75e6b26f28acdd8bf
Matt Hill <mattnine@protonmail.com>
2025-05-10 07:39:21 -0600

    init store

--- startos/init.ts
+++ startos/init.ts
@@ -4,9 +4,12 @@ import { setInterfaces } from './interfaces'
 import { versions } from './versions'
 import { actions } from './actions'
 import { setLightning } from './actions/setLightning'
+import { store } from './file-models/store.json'
 
 // **** PreInstall ****
-const preInstall = sdk.setupPreInstall(async ({ effects }) => {})
+const preInstall = sdk.setupPreInstall(async ({ effects }) => {
+  store.write(effects, { LN_BACKEND_TYPE: null })
+})
 
 // **** PostInstall ****
 const postInstall = sdk.setupPostInstall(async ({ effects }) => {
