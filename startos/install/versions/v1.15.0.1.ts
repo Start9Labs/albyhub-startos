9ec9e8cd48ffa574e934f02088758c2c0f9f95d0
Matt Hill <mattnine@protonmail.com>
2025-05-20 22:31:50 -0600

    robust migration

--- startos/install/versions/v1.15.0.1.ts
+++ startos/install/versions/v1.15.0.1.ts
@@ -1,5 +1,5 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
-import { readFile, rmdir } from 'fs/promises'
+import { readFile, rm } from 'fs/promises'
 import { load } from 'js-yaml'
 import { store } from '../../fileModels/store.json'
 
@@ -13,12 +13,14 @@ export const v_1_15_0_1 = VersionInfo.of({
         await readFile('/data/start9/config.yaml', 'utf-8'),
       ) as { lightning?: 'lnd' | 'ldk' } | undefined
 
-      const LN_BACKEND_TYPE = configYaml?.lightning === 'ldk' ? 'LDK' : 'LND'
-
-      await store.write(effects, { LN_BACKEND_TYPE })
+      if (configYaml?.lightning) {
+        await store.write(effects, {
+          LN_BACKEND_TYPE: configYaml.lightning === 'ldk' ? 'LDK' : 'LND',
+        })
+      }
 
       // remove old start9 dir
-      await rmdir('/data/start9')
+      await rm('/data/start9', { recursive: true }).catch(console.error)
     },
     down: IMPOSSIBLE,
   },
