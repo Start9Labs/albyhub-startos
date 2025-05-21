554e116312f6a5f9d4371fd118a83465a37541ba
Matt Hill <mattnine@protonmail.com>
2025-05-20 22:09:45 -0600

    beta 23 and always require node

--- startos/init/taskSetLightning.ts
+++ startos/init/taskSetLightning.ts
@@ -2,9 +2,9 @@ import { setLightning } from '../actions/setLightning'
 import { store } from '../fileModels/store.json'
 import { sdk } from '../sdk'
 
-export const taskSetLightning = sdk.setupOnInstall(async (effects) => {
+export const taskSetLightning = sdk.setupOnInit(async (effects) => {
   if (!(await store.read((s) => s.LN_BACKEND_TYPE).const(effects))) {
-    await sdk.action.requestOwn(effects, setLightning, 'critical', {
+    await sdk.action.createOwnTask(effects, setLightning, 'critical', {
       reason: 'Choose which lightning node Alby Hub will connect to',
     })
   }
