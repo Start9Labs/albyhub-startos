1bd14de5f8849b526907b4c116dc19291b2b5684
Shadowy Super Coder <musashidisciple@proton.me>
2025-07-10 12:10:55 -0600

    fix incorrect dependency mount paths

--- startos/main.ts
+++ startos/main.ts
@@ -43,7 +43,7 @@ export const main = sdk.setupMain(async ({ effects, started }) => {
       // @TODO Aiden how to get LND GRPC port?
       LND_ADDRESS: `lnd.startos:${lndgrpc?.addressInfo?.internalPort || '10009'}`,
       LND_CERT_FILE: '/mnt/lnd/tls.cert',
-      LND_MACAROON_FILE: '/mnt/lnd/admin.macaroon',
+      LND_MACAROON_FILE: '/mnt/lnd/data/chain/bitcoin/mainnet/admin.macaroon',
       ENABLE_ADVANCED_SETUP: 'false',
       ...env,
     }
@@ -52,7 +52,7 @@ export const main = sdk.setupMain(async ({ effects, started }) => {
       dependencyId: 'lnd',
       volumeId: 'main',
       subpath: null,
-      mountpoint: '/lnd',
+      mountpoint: '/mnt/lnd',
       readonly: true,
     })
   }
