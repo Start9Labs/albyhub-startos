68df70df37ce748012a2fc8cba1ac215788a4124
Shadowy Super Coder <musashidisciple@proton.me>
2025-07-10 12:26:28 -0600

    fix migration

--- startos/install/versions/v1.17.2.2-alpha.0.ts
+++ startos/install/versions/v1.17.2.2-alpha.0.ts
@@ -1,32 +1,10 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
-import { readFile, rm } from 'fs/promises'
-import { load } from 'js-yaml'
-import { store } from '../../fileModels/store.json'
 
 export const v_1_17_2_2 = VersionInfo.of({
   version: '1.17.2:2-alpha.0',
   releaseNotes: 'Updated for StartOS 0.4.0',
   migrations: {
-    up: async ({ effects }) => {
-      // get old config.yaml
-      const configYaml = load(
-        await readFile(
-          '/media/startos/volumes/main/start9/config.yaml',
-          'utf-8',
-        ),
-      ) as { lightning?: 'lnd' | 'ldk' } | undefined
-
-      if (configYaml?.lightning) {
-        await store.write(effects, {
-          LN_BACKEND_TYPE: configYaml.lightning === 'ldk' ? 'LDK' : 'LND',
-        })
-      }
-
-      // remove old start9 dir
-      await rm('/media/startos/volumes/main/start9', { recursive: true }).catch(
-        console.error,
-      )
-    },
+    up: async ({ effects }) => {},
     down: IMPOSSIBLE,
   },
 })
