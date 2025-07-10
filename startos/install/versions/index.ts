dd39ab5bde1ad44c976e4e0cb00829cb10a3e16f
Shadowy Super Coder <musashidisciple@proton.me>
2025-07-10 12:21:41 -0600

    bump version and add pre-release tag

--- startos/install/versions/index.ts
+++ startos/install/versions/index.ts
@@ -1,2 +1,4 @@
-export { v_1_17_2_1 as current } from './v1.17.2.1'
-export const other = []
+export { v_1_17_2_2 as current } from './v1.17.2.2-alpha.0'
+import { v_1_17_2_1 } from './v1.17.2.1'
+
+export const other = [v_1_17_2_1]
