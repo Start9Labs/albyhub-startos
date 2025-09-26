a6995ac68342957ed9afa9187eb6f2b6b5eb14c8
Mariusz Kogen <k0gen@pm.me>
2025-09-26 12:44:32 +0200

    Upstream 1.20.0 update

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration =
-  compat.migrations.fromMapping({}, "1.19.3");
+  compat.migrations.fromMapping({}, "1.20.0");
