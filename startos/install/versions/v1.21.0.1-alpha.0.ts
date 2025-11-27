1c0c5e173b79a3723ca748cd10e634d98322253e
Mariusz Kogen <k0gen@pm.me>
2025-11-27 11:04:22 +0100

    Icon design change

--- startos/install/versions/v1.21.0.1-alpha.0.ts
+++ startos/install/versions/v1.21.0.1-alpha.0.ts
@@ -1,11 +1,12 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const v_1_21_0_1 = VersionInfo.of({
-  version: '1.21.0:1-alpha.2',
+  version: '1.21.0:1-alpha.3',
   releaseNotes: `\
 ## Updated for StartOS 0.4.0
 
 ### Dependency Updates
+* New logo
 * Updated SDK to beta.44
 * Updated interface name for backwards compatibility
 * Updated dev dependencies to latest versions
