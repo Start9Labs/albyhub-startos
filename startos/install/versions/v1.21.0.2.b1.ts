51816f95ffbfb277c31607b60d2c2d8415ca8494
Matt Hill <mattnine@protonmail.com>
2025-12-21 18:07:57 -0700

    better migration

--- startos/install/versions/v1.21.0.2.b1.ts
+++ startos/install/versions/v1.21.0.2.b1.ts
@@ -1,6 +1,5 @@
-import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
+import { IMPOSSIBLE, VersionInfo, YAML } from '@start9labs/start-sdk'
 import { readFile, rm } from 'fs/promises'
-import { load } from 'js-yaml'
 import { storeJson } from '../../fileModels/store.json'
 
 export const v_1_21_0_2_b1 = VersionInfo.of({
@@ -9,18 +8,16 @@ export const v_1_21_0_2_b1 = VersionInfo.of({
   migrations: {
     up: async ({ effects }) => {
       // get old config.yaml
-      const raw = await readFile(
-        '/media/startos/volumes/main/start9/config.yaml',
-        'utf-8',
-      ).catch(console.log)
+      const configYaml: { lightning?: 'lnd' | 'ldk' } | undefined =
+        await readFile(
+          '/media/startos/volumes/main/start9/config.yaml',
+          'utf-8',
+        ).then(YAML.parse, () => undefined)
 
-      if (raw) {
-        const configYaml = load(raw) as { lightning?: 'lnd' | 'ldk' }
-        if (configYaml.lightning) {
-          await storeJson.write(effects, {
-            LN_BACKEND_TYPE: configYaml.lightning === 'ldk' ? 'LDK' : 'LND',
-          })
-        }
+      if (configYaml) {
+        await storeJson.write(effects, {
+          LN_BACKEND_TYPE: configYaml.lightning === 'ldk' ? 'LDK' : 'LND',
+        })
 
         // remove old start9 dir
         await rm('/media/startos/volumes/main/start9', {
