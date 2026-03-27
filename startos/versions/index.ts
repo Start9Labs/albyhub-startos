03af7e16182a42991e6d87bbafb933648d1dfdbc
Matt Hill <mattnine@protonmail.com>
2026-03-26 22:20:49 -0600

    fix lnd health check

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,7 +1,7 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_1_21_6_0_b3 } from './v1.21.6.0.b3'
+import { v_1_21_6_0_b4 } from './v1.21.6.0.b4'
 
 export const versionGraph = VersionGraph.of({
-  current: v_1_21_6_0_b3,
+  current: v_1_21_6_0_b4,
   other: [],
 })
